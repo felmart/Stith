89e956e6577ad21020c59bdf91c040f63ee224bc
stevemats <30528167+stevemats@users.noreply.github.com>
2018-09-15 16:22:43 +0300

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,51 @@
+#Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
+#Type "copyright", "credits" or "license()" for more information.
+     ##########################################
+      ##                                                                   ##
+   ##   ```````Humanoid Architecture````````                    ##
+      ##                                                                   ##
+     ##########################################
+
+import random
+from typing import Any, Union
+
+symbol = 0
+lower = 0
+upper = 0
+number = 0
+count = 0
+password = []
+
+length = input("Input Length of your choice: ")
+length = int(length)
+
+while count < length:
+    rand: Union[int, Any] = random.randint (0,3)
+    if rand == 0:
+        lower += 1
+        b = int(random.randint (97,123))
+        password.append(b)
+    elif rand == 1:
+        upper += 1
+        b = random.randint (65,91)
+        password.append(b)
+    elif rand == 2:
+        number += 1
+        b = random.randint (48,58)
+        password.append(b)
+    elif rand == 3:
+        r = random.randint(0,2)
+        symbol += 1
+        if r == 0:
+            b = random.randint (33,48)
+            password.append(b)
+        elif r == 1:
+            b = random.randint (91,97)
+            password.append(b)
+        elif r == 2:
+            b = random.randint (123,126)
+            password.append(b)
+    count += 1
+password = "".join([chr(c) for c in password])
+
+print (password)
