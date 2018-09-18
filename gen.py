dc7a70af0fbcd1d6192ff638da1ec4ae52bf4304
stevemats <30528167+stevemats@users.noreply.github.com>
2018-09-19 00:08:31 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,10 +1,9 @@
-#Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
-#Type "copyright", "credits" or "license()" for more information.
-     ##########################################
-      ##                                                                   ##
-   ##   ```````Humanoid Architecture````````                    ##
-      ##                                                                   ##
-     ##########################################
+#Python 3.7.
+            ##########################################
+      ##                                                       ##
+  ##        ```````Humanoid Architecture````````           ##
+      ##                                                       ##
+           ##########################################
 
 import random
 from typing import Any, Union
