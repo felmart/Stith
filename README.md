5737629acf69ce92c5f4e0ebbf5c39fd4a70940e
Stevemats <stevematindi@gmail.com>
2018-11-16 21:01:50 +0300

    added procedure

--- README.md
+++ README.md
@@ -14,3 +14,33 @@ knowledge inorder to use it. This program on a base level is built for:
 
 ### Installation
 
+Clone or directly download Stith to a directory of your choice.
+
+#### Running the application on linux:
+  
+  - on your favourite terminal change directory using the cd command
+    to be in the root directory of Stith, to view it use the ls command
+     and simply run the following:
+  ```
+   > python3 gen.py {hit enter}
+   
+    (specify the strength and hit enter to generate password}
+    
+  ```
+  
+ #### Running the application on windows:
+ 
+  - Change your current directory to Stith..you can 'cd' to the folder
+   and confirm being in the folder  using the 'dir' command to confirm 
+   the presence of gen.py. Now run the following:
+   
+   ```
+   > python gen.py {assuming python 3 is in path and default}
+                   or
+     python3 gen.py {for those having both 2 and 3 to avoid conflict}
+                    next 
+     (simply specify the strength of the password e.g 32 to generate pass)
+      
+   ```
+   
+   
--- gen.py
+++ gen.py
@@ -16,7 +16,7 @@ print('''
     ''')
 
 init(autoreset=True)
-f = "\n"*1
+f = "\n" * 1
 
 symbol = 0
 lower = 0
@@ -26,43 +26,44 @@ count = 0
 password = []
 
 try:
-    length = raw_input("Input a Length of your choice(e.g 5,34 ): " )
+    length = input("Input a Length of your choice(e.g 5,34 ): ")
     length = int(length)
 except NameError:
-    length = input("Input a Length of your choice(e.g 5,34 ): ")
-    length = int(length) 
+    length = raw_input("Input a Length of your choice(e.g 5,34 ): ")
+    length = int(length)
 
 while count < length:
-    rand: Union[int, Any] = random.randint (0,3)
+    rand: Union[int, Any] = random.randint(0, 3)
     if rand == 0:
         lower += 1
-        b = int(random.randint (97,123))
+        b = int(random.randint(97, 123))
         password.append(b)
     elif rand == 1:
         upper += 1
-        b = random.randint (65,91)
+        b = random.randint(65, 91)
         password.append(b)
     elif rand == 2:
         number += 1
-        b = random.randint (48,58)
+        b = random.randint(48, 58)
         password.append(b)
     elif rand == 3:
-        r = random.randint(0,2)
+        r = random.randint(0, 2)
         symbol += 1
         if r == 0:
-            b = random.randint (33,48)
+            b = random.randint(33, 48)
             password.append(b)
         elif r == 1:
-            b = random.randint (91,97)
+            b = random.randint(91, 97)
             password.append(b)
         elif r == 2:
-            b = random.randint (123,126)
+            b = random.randint(123, 126)
             password.append(b)
     count += 1
 password = "".join([chr(c) for c in password])
 
 print("\n")
-dean= "Congrats! Your new password is:   "
+dean = "Congrats! Your new password is:   "
 
-print(dean +Fore.RED + password + f)
-print(f+Style.BRIGHT + Fore.BLUE + '~~ Keep on changing your passwords on a timely based to keep off Intruders! ~~',"\t" )
+print(dean + Fore.RED + password + f)
+print(f + Style.BRIGHT + Fore.BLUE + '~~ Keep on changing your passwords on a timely based to keep off Intruders! ~~',
+      "\t")
