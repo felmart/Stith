e05cbc8b0f145a98255a79e8e19baed1560621dc
Stevemats <stevematindi@gmail.com>
2021-09-13 12:56:02 +0300

    Minor update: UX improvement

--- gen.py
+++ gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 3.7
+#!/usr/bin/env python 3
 import random
 from typing import Any, Union
 from builtins import input
@@ -7,7 +7,7 @@ try:
     import coloroma
 except ImportError:
     import os
-    print ("colorama isn\'t installed, installing now.")
+    print ("colorama isn\'t installed, installing now...")
     os.system('python -m pip install --user colorama')
     print ('colorama has been installed, restart Stith.')
 
@@ -16,63 +16,88 @@ from colorama import Fore, Style
 
 print('''                                                                               
 
-                                   |                                                           
-                                   |                                                           
-                              ___-/_\-___                                                      
-                   _____________/( . )\_____________                                           
-                  *    |    |  (  \_/  )  |    |    *                                          
-                      *|*  *|*  \_-+-_/  *|*  *|*                                              
+                                    |                                                           
+                                    |                                                           
+                                ___-/_\-___                                                      
+                    _____________/( . )\_____________                                           
+                    *    |    |  (  \_/  )  |    |    *                                          
+                        *|*  *|*  \_-+-_/  *|*  *|*                                              
+
+                    ~ Script By: @Stevemats ~
+''')
+
+def main():
+    print('\n1. Get A New Password')
+    print('2. Exit')
+    while True:
+        try:
+            choice = int(input('Enter choice: '))
+            if choice == 1:
+                pass_gen()
+                break
+            elif choice == 2:
+                print("\n"+"GoodBye!")
+                break
+            else:
+                print("Invalid choice. Enter a choice in menu. 1 or 2")
+                main()
+        except ValueError:
+            print("Invalid choice. Enter 1 or 2")
+    exit()
 
 
-    ''')
+def pass_gen():
 
-init(autoreset=True)
-f = "\n" * 1
+    init(autoreset=True)
+    f = "\n" * 1
 
-symbol = 0
-lower = 0
-upper = 0
-number = 0
-count = 0
-password = []
+    symbol = 0
+    lower = 0
+    upper = 0
+    number = 0
+    count = 0
+    password = []
 
-try:
-    length = int(input("Input a Length of your choice(e.g 5,34 ): "))
-except NameError:
-    assert isinstance(length, int) # native
+    try:
+        length = int(input("Input Password Length of your choice(e.g 12 ): "))
+    except NameError:
+        assert isinstance(length, int)
 
-while count < length:
-    rand: Union[int, Any] = random.randint(0, 3)
-    if rand == 0:
-        lower += 1
-        b = int(random.randint(97, 123))
-        password.append(b)
-    elif rand == 1:
-        upper += 1
-        b = random.randint(65, 91)
-        password.append(b)
-    elif rand == 2:
-        number += 1
-        b = random.randint(48, 58)
-        password.append(b)
-    elif rand == 3:
-        r = random.randint(0, 2)
-        symbol += 1
-        if r == 0:
-            b = random.randint(33, 48)
+    while count < length:
+        rand: Union[int, Any] = random.randint(0, 3)
+        if rand == 0:
+            lower += 1
+            b = int(random.randint(97, 123))
             password.append(b)
-        elif r == 1:
-            b = random.randint(91, 97)
+        elif rand == 1:
+            upper += 1
+            b = random.randint(65, 91)
             password.append(b)
-        elif r == 2:
-            b = random.randint(123, 126)
+        elif rand == 2:
+            number += 1
+            b = random.randint(48, 58)
             password.append(b)
-    count += 1
-password = "".join([chr(c) for c in password])
+        elif rand == 3:
+            r = random.randint(0, 2)
+            symbol += 1
+            if r == 0:
+                b = random.randint(33, 48)
+                password.append(b)
+            elif r == 1:
+                b = random.randint(91, 97)
+                password.append(b)
+            elif r == 2:
+                b = random.randint(123, 126)
+                password.append(b)
+        count += 1
+    password = "".join([chr(c) for c in password])
 
-print("\n")
-dean = "Congrats! Your new password is:   "
+    print("\n")
+    dean = "Your new password is:   "
 
-print(dean + Fore.RED + password + f)
-print(f + Style.BRIGHT + Fore.BLUE + '~~ Keep on changing your passwords on a timely based to keep off Intruders! ~~',
-      "\t")
+    print(dean + Fore.GREEN + password + f)
+    print(f + Style.BRIGHT + Fore.BLUE + '~~ Keep on changing your passwords on a timely based to keep off Intruders! ~~',
+        "\t")
+
+if __name__ == '__main__':
+    main()
